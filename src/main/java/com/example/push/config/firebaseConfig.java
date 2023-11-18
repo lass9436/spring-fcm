fb0f216c412fc29429c1e2aaf76447cf10506b15
lass9436 <lass9436@gmail.com>
2023-11-18 11:00:13 +0900

    spring boot init & firebase config

---
+++ src/main/java/com/example/push/config/firebaseConfig.java
@@ -0,0 +1,25 @@
+package com.example.push.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class firebaseConfig {
+
+    @Bean
+    public FirebaseApp firbaseApp() throws IOException {
+        ClassPathResource resource = new ClassPathResource("firebase/firebaseKey.json");
+        InputStream token = resource.getInputStream();
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(token))
+                .build();
+        return FirebaseApp.initializeApp(options);
+    }
+}
